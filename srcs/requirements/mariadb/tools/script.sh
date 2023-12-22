2be411a4b8ac34e190b8e503671225e8eeaba67a
Ayoubioussoufa <ayoubioussoufa@gmail.com>
2023-12-22 11:21:50 +0100

    pff

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,8 +1,17 @@
 service mysql start
 
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS mariadb; \
-                    CREATE USER 'me'@'%' IDENTIFIED BY '12345'; \
-                    GRANT ALL PRIVILEGES ON mariadb.* TO 'me'@'%'; \
-                    FLUSH PRIVILEGES;"
+sleep 2
 
-exec $@
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+mysql -u root -e  "CREATE USER IF NOT EXISTS '${DBUSER}'@'%' IDENTIFIED BY '${DBUSERPASS}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '${DBUSER}'@'%';FLUSH PRIVILEGES;"
+
+
+mysqladmin -u root shutdown
+
+echo "database have been created..."
+
+mysqld_safe
+
+
+# https://youtu.be/xiUTqnI6xk8?si=Jw3DDYc0JuNJbTGE
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,11 +1,19 @@
-cd /var/www/html/wordpress
-#Downloading and extracting Wordpress core files to the current directory
-wp core download --allow-root
+cd wordpress
+cat /var/www/wordpress/wp-config-sample.php > wp-config.php
 
-# Creating the wp-config.php file using this command.
-wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
+sed -i "s/database_name_here/wordpress/" wp-config.php 
+sed -i "s/username_here/${DBUSER}/" wp-config.php
+sed -i "s/password_here/${DBUSERPASS}/" wp-config.php
+sed -i "s/localhost/database/" wp-config.php
 
-# Installing wordpress using the given environment variables to avoid showing the wordpress installation page everytime we run the containe
-wp core install --url=${DOMAIN_NAME} --title=DopamInception --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --a
 
-exec $@
+
+wp user create ${USER} ${USEREMAIL} --role=author --user_pass=${USERPASS}  --allow-root
+wp user create aybiouss test@gmail.com --role=author --user_pass=aybiouss/1337  --allow-root
+
+wp core install --url=localhost --title=web-site --admin_user=${ADMIN} --admin_password=${ADMINPASS} --admin_email=${ADMINEMAIL} --allow-root
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+
+php-fpm7.3 -F
