f9dc678048c468327dca48f8d316e33f1a91da33
ayoubioussoufa <ayoubioussoufa@gmail.com>
2024-07-01 18:22:49 +0100

    Fixing after 6months lol

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,8 +3,8 @@ service mysql start
 sleep 2
 
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
-mysql -u root -e  "CREATE USER IF NOT EXISTS '${DBUSER}'@'%' IDENTIFIED BY '${DBUSERPASS}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '${DBUSER}'@'%';FLUSH PRIVILEGES;"
+mysql -u root -e  "CREATE USER IF NOT EXISTS '${DBUser}'@'%' IDENTIFIED BY '${DBUserPassword}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '${DBUser}'@'%';FLUSH PRIVILEGES;"
 
 
 mysqladmin -u root shutdown
@@ -15,3 +15,11 @@ mysqld_safe
 
 
 # https://youtu.be/xiUTqnI6xk8?si=Jw3DDYc0JuNJbTGE
+
+# service mysql start
+
+# sleep 1
+
+# mariadb -u root -e "CREATE DATABASE '${DBName}' ; CREATE USER '${DBUser}'@'%' IDENTIFIED BY '${DBUserPassword}'; REQUIRE NOT REGEXP 'admin|Admin|administrator|Administrator'; GRANT ALL PRIVILEGES ON '${DBName}'.* TO '${DBUser}'@'%'; FLUSH PRIVILEGES; ALTER USER 'root'@'localhost' IDENTIFIED BY '${DBRootPassword}'; FLUSH PRIVILEGES;"
+
+# exec $@
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,19 +1,31 @@
-cd wordpress
-cat /var/www/wordpress/wp-config-sample.php > wp-config.php
+# cd wordpress
+# cat /var/www/wordpress/wp-config-sample.php > wp-config.php
 
-sed -i "s/database_name_here/wordpress/" wp-config.php 
-sed -i "s/username_here/${MYSQL_USER}/" wp-config.php
-sed -i "s/password_here/${MYSQL_PASSWORD}/" wp-config.php
-sed -i "s/localhost/database/" wp-config.php
-
-
-
-wp user create ${MYSQL_USER} ${MYSQL_USER_EMAIL} --role=author --user_pass=${MYSQL_PASSWORD}  --allow-root
-wp user create aybiouss test@gmail.com --role=author --user_pass=aybiouss/1337  --allow-root
-
-wp core install --url=localhost --title=web-site --admin_user=${MYSQL_DATABASE} --admin_password=${MYSQL_ROOT_PASSWORD} --admin_email=${MYSQL_DATEBASE_EMAIL} --allow-root
-
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+# sed -i "s/database_name_here/wordpress/" wp-config.php 
+# sed -i "s/username_here/${MYSQL_USER}/" wp-config.php
+# sed -i "s/password_here/${MYSQL_PASSWORD}/" wp-config.php
+# sed -i "s/localhost/database/" wp-config.php
 
 
-php-fpm7.3 -F
+
+# wp user create ${MYSQL_USER} ${MYSQL_USER_EMAIL} --role=author --user_pass=${MYSQL_PASSWORD}  --allow-root
+# wp user create aybiouss test@gmail.com --role=author --user_pass=aybiouss/1337  --allow-root
+
+# wp core install --url=localhost --title=web-site --admin_user=${MYSQL_DATABASE} --admin_password=${MYSQL_ROOT_PASSWORD} --admin_email=${MYSQL_DATEBASE_EMAIL} --allow-root
+
+# sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+
+# php-fpm7.3 -F
+
+cd /var/www/html/wordpress
+
+#Downloading and extracting Wordpress core files to the current directory
+wp core download --allow-root
+
+# Creating the wp-config.php file using this command.
+wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
+
+# Installing wordpress using the given environment variables to avoid showing the wordpress installation page everytime we run the containe
+wp core install --url=${DOMAIN_NAME} --title=DopamInception --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --a
+exec $@
