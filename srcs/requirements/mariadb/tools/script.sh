eeace87dca2d84a4359ead7cfae7346ba9f8c20c
ayoubioussoufa <ayoubioussoufa@gmail.com>
2024-07-02 15:50:12 +0100

    slowly but surely

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,25 +1,20 @@
-service mysql start
+#!/bin/bash
 
-sleep 2
+service mariadb start
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
-mysql -u root -e  "CREATE USER IF NOT EXISTS '${DBUser}'@'%' IDENTIFIED BY '${DBUserPassword}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '${DBUser}'@'%';FLUSH PRIVILEGES;"
+sleep 3
 
+mariadb  -e "CREATE DATABASE IF NOT EXISTS $DBName;"
+mariadb -e "CREATE USER IF NOT EXISTS '$DBUser'@'%' IDENTIFIED BY '$DBUserPassword';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`$DBName\`.* TO '$DBUser'@'%';"
+mariadb -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root shutdown
+service mariadb stop
 
 echo "database have been created..."
 
+sleep 2
+
 mysqld_safe
 
-
 # https://youtu.be/xiUTqnI6xk8?si=Jw3DDYc0JuNJbTGE
-
-# service mysql start
-
-# sleep 1
-
-# mariadb -u root -e "CREATE DATABASE '${DBName}' ; CREATE USER '${DBUser}'@'%' IDENTIFIED BY '${DBUserPassword}'; REQUIRE NOT REGEXP 'admin|Admin|administrator|Administrator'; GRANT ALL PRIVILEGES ON '${DBName}'.* TO '${DBUser}'@'%'; FLUSH PRIVILEGES; ALTER USER 'root'@'localhost' IDENTIFIED BY '${DBRootPassword}'; FLUSH PRIVILEGES;"
-
-# exec $@
