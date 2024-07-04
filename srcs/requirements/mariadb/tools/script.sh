65e6c45ef44a8a36644d64538ef48b8a07812875
ayoubioussoufa <ayoubioussoufa@gmail.com>
2024-07-04 11:59:04 +0100

    nicely done

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,7 +2,6 @@ iservice mariadb start
 
 sleep 3
 
-
 mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mariadb -e "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';"
@@ -14,4 +13,4 @@ echo "database have been created..."
 
 sleep 2
 
-mysqld_safe
+exec mariadbd_safe
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,4 +1,4 @@
-# #!/bin/bash
+#!/bin/bash
 
 # # Wait for the database to be ready
 # sleep 10
@@ -31,25 +31,64 @@
 # # Start PHP-FPM
 # php-fpm8.2 -F
 
-sleep 10
+# sleep 10
 
+# cd /var/www/wordpress
+
+# if [ -f "wp-config.php" ]; then
+# 	echo  "WordPress is already installed."
+# else
+#     wp core install --url="http://localhost" --title=web-site --admin_user=${ADMIN} --admin_password=${ADMINPASS} --admin_email=${ADMINEMAIL} --allow-root
+#     cat /var/www/wordpress/wp-config-sample.php > wp-config.php
+
+#     sed -i "s/database_name_here/${WORDPRESS_DB_NAME}/" wp-config.php 
+#     sed -i "s/username_here/${WORDPRESS_DB_USER}/" wp-config.php
+#     sed -i "s/password_here/${WORDPRESS_DB_PASSWORD}/" wp-config.php
+#     sed -i "s/localhost/${WORDPRESS_DB_HOST}/" wp-config.php
+# fi
+
+# wp user create ${MYSQL_USER} ${MYSQL_USER_EMAIL} --role=author --user_pass=${MYSQL_PASSWORD}  --allow-root
+
+# sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
+
+
+# php-fpm8.2 -F
+
+# cd /var/www/wordpress
+# #Downloading and extracting Wordpress core files to the current directory
+# wp core download --allow-root
+# # Creating the wp-config.php file using this command.
+# wp core config --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${WORDPRESS_DB_HOST} --allow-root
+# # Installing wordpress using the given environment variables to avoid showing the wordpress installation page everytime we run the containe
+# wp core install --url="http://localhost" --title=DopamInception --admin_user=${ADMIN} --admin_password=${ADMINPASS} --admin_email=${ADMINEMAIL} --a
+
+# sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
+
+# php-fpm8.2 -F
+echo PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP
+
+mkdir -p /var/www/wordpress
 cd /var/www/wordpress
 
-if [ -f "wp-config.php" ]; then
-	echo  "WordPress is already installed."
-else
-    cat /var/www/wordpress/wp-config-sample.php > wp-config.php
+echo salam > plop
 
-    sed -i "s/database_name_here/${WORDPRESS_DB_NAME}/" wp-config.php 
-    sed -i "s/username_here/${WORDPRESS_DB_USER}/" wp-config.php
-    sed -i "s/password_here/${WORDPRESS_DB_PASSWORD}/" wp-config.php
-    sed -i "s/localhost/${WORDPRESS_DB_HOST}/" wp-config.php
-fi
-
-wp core install --url="http://localhost" --title=web-site --admin_user=${ADMIN} --admin_password=${ADMINPASS} --admin_email=${ADMINEMAIL} --allow-root
-wp user create ${MYSQL_USER} ${MYSQL_USER_EMAIL} --role=author --user_pass=${MYSQL_PASSWORD}  --allow-root
-
-sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
+until mariadb -u "$MYSQL_USER" -h mariadb --password="${MYSQL_PASSWORD}" -e "show databases;"; do
+  echo "Database is not up yet. Waiting..."
+  sleep 0.2 
+done
+sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 0.0.0.0:9000|g' /etc/php/7.4/fpm/pool.d/www.conf
 
 
-php-fpm8.2 -F
+wp core download  --allow-root
+
+wp config create --allow-root  --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb 
+
+wp core install --url="http://localhost" --title=DopamInception --admin_user=${ADMIN} --admin_password=${ADMINPASS} --admin_email=${ADMINEMAIL} --allow-root
+
+wp user create ${MYSQL_USER} ${MYSQL_USER_EMAIL} --role=author --user_pass=${MYSQL_PASSWORD} --allow-root
+
+chown -R www-data:www-data /var/www/wordpress
+
+mkdir -p /run/php/
+
+exec php-fpm7.4 -F -R
